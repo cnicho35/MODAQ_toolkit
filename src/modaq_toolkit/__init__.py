94be11453c39261a32a31d9fa89cdab546d92c50
Simms, Andrew <Andrew.Simms@nrel.gov>
2025-03-25 08:47:54 -0600

    Release 0.3.0: Add dataframe dict output

--- src/modaq_toolkit/__init__.py
+++ src/modaq_toolkit/__init__.py
@@ -2,7 +2,7 @@
 MODAQ Toolkit: High-performance MODAQ to Parquet converter and analysis toolkit
 """
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 from .message_processing import (
     MessageProcessor,
